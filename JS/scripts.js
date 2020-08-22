368be616f76aae4373582f7520fac1f98e1ea605
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-08-22 17:57:29 +1200

    sample function - get location by City and display weather.

---
+++ JS/scripts.js
@@ -0,0 +1,37 @@
+// window.onload = function () {
+//   weatherBalloon("Wellington");
+// };
+
+
+function submitLocation(){
+  // alert("Hello!");
+  let city = document.getElementById("cityName").value;
+  weatherBalloon(city);
+}
+
+// API-KEY: 7b0894cd04c9e2e3e53e9d4b07464879
+function weatherBalloon(cityName) {
+  var key = "7b0894cd04c9e2e3e53e9d4b07464879";
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + key)
+    .then(function (resp) {
+      return resp.json();
+    }) // Convert data to json
+    .then(function (data) {
+      console.log(data);
+      drawWeather(data);
+    })
+    .catch(function () {
+      // catch any errors
+      alert("City Not Found!\nPlease Try Again...");
+    });
+}
+
+//----------------------------------------------//
+function drawWeather(data) {
+  var celcius = Math.round(parseFloat(data.main.temp) - 273.15);
+  var fahrenheit = Math.round((parseFloat(data.main.temp) - 273.15) * 1.8 + 32);
+
+  document.getElementById("description").innerHTML = data.weather[0].description;
+  document.getElementById("temp").innerHTML = celcius + "&deg;";
+  document.getElementById("location").innerHTML = data.name;
+}
